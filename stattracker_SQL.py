2dda89aae3aaebdbb848800a5ee1f93d8dc771db
Cobi Finkelstein <cefinkelstein@wpi.edu>
2017-10-06 01:16:53 -0400

    added insertFighter() to tracker

--- stattracker_SQL.py
+++ stattracker_SQL.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os.path
 import ConfigParser
 import mysql.connector
@@ -10,7 +11,7 @@ config.read("config.cfg")
 def vprint(s):
     """Prints a string if the verbose flag is set in config.cfg"""
     if bool(eval(config.get("General","verbose"))):
-        print s
+        print(s)
 
 # connect to mySQL server using config.cfg
 db_host = config.get("Database","host")
@@ -21,4 +22,23 @@ db_data = config.get("Database", "database")
 cnx = mysql.connector.connect(host=db_host, port=db_port,
                               user=db_user, password=db_pass,
                               database=db_data)
+cursor = cnx.cursor()
 vprint("stattracker_SQL:Connected to mySQL.")
+
+def insertFighter(s):
+    """Inserts a fighter into the fighter table."""
+    statement = ("INSERT IGNORE INTO fighter\n" +
+                 "(name)\n" +
+                 "VALUES\n" +
+                 "('" + s + "');")
+    vprint("stattracker_SQL:\n" +
+           statement)
+    cursor.execute(statement)
+
+def cleanDB():
+    """Cleans the database. You shouldn't have delete access unless you're an admin,
+        so if this works for you I really should blame myself."""
+
+def closeDB():
+    cnx.close()
+    cursor.close()
